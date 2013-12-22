2f2709b8f2ee40acf89dffc6d9dc68b3c9c1ca90
Mitchell Fukumoto <mitchsf777@gmail.com>
2013-12-22 02:53:11 -1000

    yeah right...

--- taikoexplorer/settings.py
+++ taikoexplorer/settings.py
@@ -199,6 +199,8 @@ ALLOWED_HOSTS = ['*']
 import os
 local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
 
+PIPELINE_YUGLIFY_BINARY = local_path('../node_modules/yuglify/bin/yuglify')
+
 PIPELINE_CSS = {
   'all': {
     'source_filenames': (
